952c67c370adbbd08fd6e5971b0c3ec5cc56bc87
Prithivvj2406 <159470988+Prithivvj2406@users.noreply.github.com>
2024-06-08 17:34:21 +0530

    Add files via upload
    Milestone 2: Adding mouse click for closing the Intial Game Startup Screen

--- Main_Game_File.py
+++ Main_Game_File.py
@@ -4,23 +4,30 @@ import tkinter as tk
 WIDTH = 800
 HEIGHT = 600
 
+
 # MILESTONE_1
-# Function to create the initial Tkinter Game window
+# Function to create the initial Tkinter window
 def create_tkinter_window():
     root = tk.Tk()  # Create the main application window
     root.title("Breakout Game")  # Set the title of the window
     root.geometry(f"{WIDTH}x{HEIGHT}")  # Position the window
 
     # Add an instruction label to the center of the window
-    instruction_label = tk.Label(root, text="Game Startup Screen", font=("Helvetica", 16))
+    instruction_label = tk.Label(root, text="CLICK ANYWHERE ON THE SCREEN TO START THE GAME", font=("Helvetica", 16))
     instruction_label.place(relx=0.5, rely=0.5, anchor="center")
+
+    # MILESTONE_2
+    # Add click for closing the screen
+    root.bind("<Button-1>", lambda event: root.destroy())
 
     # Run the Tkinter event loop
     root.mainloop()
 
+
 # Main function to start the game setup
 def main():
-    create_tkinter_window()  # MILESTONE 1
+    create_tkinter_window()  # MILESTONE 1 & 2
+
 
 # Entry point of the program
 if __name__ == "__main__":
